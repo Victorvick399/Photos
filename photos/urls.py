88258fa7e9a0012ad786c8909730c134fe0c3c5b
Victorvick399 <victorvick399@gmail.com>
2019-12-16 09:06:00 +0300

    created url links

--- photos/urls.py
+++ photos/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.home_page,name='home'),
-    url(r'^about/$',views.about_page,name='about')
+    url(r'^about/$',views.about_page,name='about'),
+    url(r'^search/', views.search_results, name='search_results')
     ]
