64e21bee9fd5c5f131f50354f793ab352554ca5c
Victorvick399 <victorvick399@gmail.com>
2019-12-16 09:16:42 +0300

    create views to display pictures

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from .models import Photos,Category,Location
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    all_pics=Photos.objects.all()
+    return render(request, 'home.html' ,{"photos":all_pics})
 
 def about_page(request):
     return render(request, 'test.html')
+
+def search_results(request):
+
+    if 'photos' in request.GET and request.GET["photos"]:
+        search_term = request.GET.get("photos")
+        searched_photos = Photos.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
