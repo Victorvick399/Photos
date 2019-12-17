716bb7c57bd737b258a5caf9483d9e0e2603040b
Victorvick399 <victorvick399@gmail.com>
2019-12-17 08:28:29 +0300

    add searcg by lication and image id

--- photos/models.py
+++ photos/models.py
@@ -2,10 +2,14 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
-
-
 class Location(models.Model):
     name = models.CharField(max_length=30)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
 
     def __str__(self):
         return self.name
@@ -13,6 +17,12 @@ class Location(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
 
     def __str__(self):
         return self.name
@@ -33,8 +43,20 @@ class Photos(models.Model):
 
     @classmethod
     def search_by_category(cls, search_term):
-        pic_results=cls.objects.filter(category__icontains=search_term)
+        category = Category.objects.filter(name__icontains=search_term)
+        pic_results=cls.objects.filter(category_id=category)
         return pic_results
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = Photos.objects.filter(id=id)
+        return image
+
+    @classmethod
+    def filter_by_location(cls,location):
+        loc=Location.objects.filter(name__icontains=location)
+        p_results=cls.objects.filter(location_id=loc)
+        return p_results
 
     class Meta:
         ordering = ['name']
