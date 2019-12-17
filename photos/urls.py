41f7176b33190bb558d74a0ce04f9f0434856ad7
Victorvick399 <victorvick399@gmail.com>
2019-12-17 08:30:49 +0300

    add url to the search page

--- photos/urls.py
+++ photos/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
@@ -7,3 +9,6 @@ urlpatterns=[
     url(r'^about/$',views.about_page,name='about'),
     url(r'^search/', views.search_results, name='search_results')
     ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
